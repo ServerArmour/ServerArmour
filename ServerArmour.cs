fd341b8f75b214322a1907bf18e6b50492ebee6a
Shaun Jansen Van Nieuwenhuizen (Master) <shaun@jvn.sx>
2020-01-14 23:02:05 +0200

    * Plugin baseline done * Caching implemented * Checking all users on start that isn't cached, or cache to old * Refreshin cache for each new user joining regardless of cache time. Need to make this configurable * Already querying server, need to implement the ban pushing for opensource sharing.

--- ServerArmour.cs
+++ ServerArmour.cs
@@ -1,4 +1,8 @@
-﻿using Oxide.Core.Libraries.Covalence;
+﻿using Newtonsoft.Json;
+using Oxide.Core;
+using Oxide.Core.Libraries;
+using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
 
 namespace Oxide.Plugins
 {
@@ -6,13 +10,39 @@ namespace Oxide.Plugins
     [Description("Protect your server! Auto ban hackers, and notify other server owners of hackers")]
     class ServerArmour : CovalencePlugin
     {
+        #region Variables
+        Dictionary<string, ISAPlayer> _playerData = new Dictionary<string, ISAPlayer>();
+        private Time _time = GetLibrary<Time>();
+        private double MaxMinutesToRefresh = 60;
+        #endregion
+
+        #region Hooks
         void Init()
         {
-            Puts("Init works!");
+            Puts("Server Armour is being initialized.");
+            LoadSettings();
+        }
+
+        void Loaded()
+        {
+            Puts("Checking all known users.");
+
+            int playerTotal = 0;
+            foreach (IPlayer player in players.All)
+            {
+                if (player != null)
+                    GetPlayerBans(player);
+                playerTotal++;
+            }
+
+            Puts("Checking " + playerTotal + " players");
+            Puts("Server Armour finished initializing.");
         }
 
         void OnUserConnected(IPlayer player)
         {
+            GetPlayerBans(player, true);
+
             Puts($"{player.Name} ({player.Id}) connected from {player.Address}");
 
             if (player.IsAdmin)
@@ -21,8 +51,6 @@ namespace Oxide.Plugins
             }
 
             Puts($"{player.Name} is {(player.IsBanned ? "banned" : "not banned")}");
-
-            server.Broadcast($"Welcome {player.Name} to {server.Name}!");
         }
 
         void OnUserDisconnected(IPlayer player)
@@ -65,5 +93,93 @@ namespace Oxide.Plugins
         {
             Puts($"Player {name} ({id}) at {ip} was unbanned");
         }
+        #endregion
+
+        #region WebRequests
+        void GetPlayerBans(IPlayer player, bool forceRefresh = false)
+        {
+
+            if (forceRefresh)
+            {
+                Puts("Forcing a refresh for newly connected user " + player.Name);
+            }
+            else
+            {
+                if (PlayerCached(player.Id))
+                {
+                    uint cachedTimestamp = _playerData[player.Id].cacheTimestamp;
+                    uint currentTimestamp = _time.GetUnixTimestamp();
+                    double minutesOld = (currentTimestamp - cachedTimestamp) / 60.0 / 1000.0;
+                    bool oldCache = MaxMinutesToRefresh <= minutesOld;
+                    Puts("Player " + player.Name + " exists, " + (oldCache ? "however his cache is old and will now refresh." : "and his cache is still fresh."));
+
+                    if (!oldCache)
+                        return; //user already cached, therefore do not check again before cache time laps.
+                }
+            }
+
+            string url = $"http://io.serverarmour.com/checkUser?steamid={player.Id}&username={player.Name}&ip={player.Address}";
+            Puts(url);
+            webrequest.Enqueue(url, null, (code, response) =>
+            {
+                if (code != 200 || response == null)
+                {
+                    Puts($"Couldn't get an answer from ServerArmour.com!");
+                    return;
+                }
+
+                ISAPlayer isaPlayer = JsonConvert.DeserializeObject<ISAPlayer>(response);
+                isaPlayer.cacheTimestamp = _time.GetUnixTimestamp();
+                _playerData.Add(isaPlayer.steamid, isaPlayer);
+                if (isaPlayer != null && isaPlayer.banCount > 0)
+                {
+                    Puts($"ServerArmour: {isaPlayer.username} checked, and user is dirty with a total of ***{isaPlayer.banCount}*** bans");
+                }
+                else if (isaPlayer != null)
+                {
+                    Puts($"ServerArmour: {isaPlayer.username} checked, and user is clean");
+                }
+                SaveSettings();
+            }, this, RequestMethod.GET);
+        }
+        #endregion
+        #region Classes 
+        public class ISAPlayer
+        {
+            public string steamid;
+            public string username;
+            public int banCount;
+            public List<ISABan> banData;
+            public uint cacheTimestamp;
+        }
+        public class ISABan
+        {
+            public string serverName;
+            public string reason;
+            public string date;
+        }
+        #endregion
+
+        #region Data Handling
+        bool PlayerCached(string steamid) => _playerData.ContainsKey(steamid);
+
+        void SaveSettings()
+        {
+            Interface.Oxide.DataFileSystem.WriteObject<Dictionary<string, ISAPlayer>>("ServerArmour", _playerData, true);
+        }
+
+        void LoadSettings()
+        {
+            if (Interface.Oxide.DataFileSystem.ExistsDatafile("ServerArmour"))
+            {
+                _playerData = Interface.Oxide.DataFileSystem.ReadObject<Dictionary<string, ISAPlayer>>("ServerArmour");
+                Puts("Settings loaded");
+            }
+            else
+            {
+                SaveSettings();
+            }
+        }
+        #endregion
     }
 }
