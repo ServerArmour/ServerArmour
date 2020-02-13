00e3d1926bbcbcd183b64005ab07c57b98a3b9a4
Shaun Jansen Van Nieuwenhuizen (Master) <shaun@jvn.sx>
2020-02-13 22:42:05 +0200

    ## If you get errors, then please delete your current config.

--- ServerArmour.cs
+++ ServerArmour.cs
@@ -14,7 +14,7 @@ using Time = Oxide.Core.Libraries.Time;
 
 
 namespace Oxide.Plugins {
-    [Info("ServerArmour", "Pho3niX90", "0.0.86")]
+    [Info("ServerArmour", "Pho3niX90", "0.0.87")]
     [Description("Protect your server! Auto ban known hacker, scripter and griefer accounts, and notify server owners of threats.")]
     class ServerArmour : CovalencePlugin {
 
